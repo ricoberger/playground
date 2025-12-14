66807ff4630f08c01897bed5a6c516cd7da4229c
ricoberger <mail@ricoberger.de>
2025-12-14 14:03:12 +0100

    Homelab Setup

--- homelab/README.md
+++ homelab/README.md
@@ -1,6 +1,6 @@
 # Homelab
 
-1. Create a `.env.` file for the DNS setup with the following content:
+1. Create a `.env` file for the DNS setup with the following content:
 
    ```sh
    touch dns/.env
@@ -16,9 +16,6 @@
    ```sh
    ./dns/dns.sh --domain=<VPN>.homelab.ricoberger.dev --ip=public --operation=create
 
-   ./dns/dns.sh --domain=traefik.homelab.ricoberger.dev --ip=private --operation=create
-   ./dns/dns.sh --domain=ollama.homelab.ricoberger.dev --ip=private --operation=create
-   ./dns/dns.sh --domain=open-webui.homelab.ricoberger.dev --ip=private --operation=create
    ./dns/dns.sh --domain=registry.homelab.ricoberger.dev --ip=private --operation=create
    ./dns/dns.sh --domain=kubernetes.homelab.ricoberger.dev --ip=private --operation=create
    ```
@@ -31,47 +28,15 @@
 
    ```plaintext
    */5 * * * * /Users/ricoberger/Documents/GitHub/ricoberger/playground/homelab/dns/dns.sh --domain=<VPN>.homelab.ricoberger.dev --ip=public --operation=update
+   # Or with logging output to file
+   # */5 * * * * /Users/ricoberger/Documents/GitHub/ricoberger/playground/homelab/dns/dns.sh --domain=<VPN>.homelab.ricoberger.dev --ip=public --operation=update >> /Users/ricoberger/Desktop/dns-vpn-update.log 2>&1
    ```
 
    > If the CronJob fails with an error `Operation not permitted` on macOS, go
    > to **System Settings** -> **Privacy & Security** -> **Full Disk Access**
    > and add the `/usr/sbin/cron` binary.
 
-4. Create a `.env.` file for the Traefik setup with the following content:
-
-   ```sh
-   touch traefik/.env
-   ```
-
-   ```plaintext
-   CF_DNS_API_TOKEN=
-   TRAEFIK_CERTIFICATESRESOLVERS_CLOUDFLARE_ACME_EMAIL=
-   TRAEFIK_DASHBOARD_CREDENTIALS=
-   ```
-
-5. Create a `acme.json` file for Traefik, which is used to store the
-   certificates:
-
-   ```sh
-   touch traefik/data/acme.json
-   chmod 600 traefik/data/acme.json
-   ```
-
-6. Create a network for Docker:
-
-   ```sh
-   docker network create proxy
-   ```
-
-7. Start all services:
-
-   ```sh
-   docker-compose -f traefik/docker-compose.yaml up -d --force-recreate
-   docker-compose -f open-webui/docker-compose.yaml up -d --force-recreate
-   docker-compose -f registry/docker-compose.yaml up -d --force-recreate
-   ```
-
-8. Run a Kubernetes cluster via `kind`:
+4. Start a Kubernetes cluster via `kind`:
 
    ```sh
    ./kubernetes/kind.sh
--- homelab/kubernetes/kind.sh
+++ homelab/kubernetes/kind.sh
@@ -2,6 +2,17 @@
 
 set -o errexit
 
+# Create registry container unless it already exists
+registry_name='homelab-registry'
+registry_port='5001'
+
+if [ "$(docker inspect -f '{{.State.Running}}' "${registry_name}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "0.0.0.0:${registry_port}:5000" --network bridge --name "${registry_name}" \
+    registry:2
+fi
+
+
 # Create a three node kind cluster with containerd registry config dir enabled
 # and extra port mappings for the ingress controller
 #
@@ -20,18 +31,18 @@ networking:
   apiServerPort: 6443
 nodes:
   - role: control-plane
-    image: kindest/node:v1.32.3
+    image: kindest/node:v1.34.2
     extraPortMappings:
       - containerPort: 80
-        hostPort: 8080
+        hostPort: 80
         protocol: TCP
       - containerPort: 443
-        hostPort: 8443
+        hostPort: 443
         protocol: TCP
   - role: worker
-    image: kindest/node:v1.32.3
+    image: kindest/node:v1.34.2
   - role: worker
-    image: kindest/node:v1.32.3
+    image: kindest/node:v1.34.2
 containerdConfigPatches:
   - |-
     [plugins."io.containerd.grpc.v1.cri".registry]
@@ -49,21 +60,21 @@ EOF
 # Add the registry config to the nodes
 #
 # We want a consistent name that works from both ends, so we tell containerd to
-# alias registry.homelab.ricoberger.dev to the registry container when pulling
-# images
-REGISTRY_DIR="/etc/containerd/certs.d/registry.homelab.ricoberger.dev"
+# alias registry.homelab.ricoberger.dev:${registry_port} to the registry
+# container when pulling images
+REGISTRY_DIR="/etc/containerd/certs.d/registry.homelab.ricoberger.dev:${registry_port}"
 for node in $(kind get nodes); do
   docker exec "${node}" mkdir -p "${REGISTRY_DIR}"
   cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${REGISTRY_DIR}/hosts.toml"
-[host."https://registry.homelab.ricoberger.dev"]
+[host."http://${registry_name}:5000"]
 EOF
 done
 
 # Connect the registry to the cluster network if not already connected
 # This allows kind to bootstrap the network but ensures they're on the same
 # network
-if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "registry")" = 'null' ]; then
-  docker network connect "kind" "registry"
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${registry_name}")" = 'null' ]; then
+  docker network connect "kind" "${registry_name}"
 fi
 
 # Document the local registry
@@ -78,6 +89,6 @@ metadata:
   namespace: kube-public
 data:
   localRegistryHosting.v1: |
-    host: "registry.homelab.ricoberger.dev"
+    host: "registry.homelab.ricoberger.dev:${registry_port}"
     help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
 EOF
