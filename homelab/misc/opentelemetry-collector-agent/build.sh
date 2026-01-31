9dfaa3c2de47b61907ff203e135da517dd053bac
Rico Berger <ricoberger@users.noreply.github.com>
2026-01-31 18:42:10 +0100

    Homelab: Remove ClickHouse and Prometheus

--- homelab/misc/opentelemetry-collector-agent/build.sh
+++ homelab/misc/opentelemetry-collector-agent/build.sh
@@ -7,7 +7,7 @@ set -o nounset
 docker buildx build \
   --platform=linux/arm64,linux/amd64 \
   -f ./Dockerfile \
-  -t registry.homelab.ricoberger.dev:5555/otel-collector-agent:v0.142.0 \
+  -t registry.homelab.ricoberger.dev:5555/otel-collector-agent:v0.144.0 \
   --output=type=registry,registry.insecure=true \
   --push \
   .
